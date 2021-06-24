7d5ada21341442b1029a176eade347a0025c198f
jlangy <37274633+jlangy@users.noreply.github.com>
2021-06-24 03:02:20 +0000

    Deploying to gh-pages from @ jlangy/next-gh-pages-template@14af52e5e5077af311f2a5080cf2ecc0cd71aa5d 🚀

---
+++ _next/static/YumwF1pboAAqBDflI8EJH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-3b2f0b3a88598e3a0848.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
