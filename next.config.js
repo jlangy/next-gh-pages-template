744b94a8b223e490e36bf2a7330b6b44748c6bbc
Jonathan Langlois <jon@button.is>
2021-06-24 18:06:21 -0700

    ap test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: 'https://jlangy.github.io/next-gh-pages-template/',
+  assetPrefix: isProd ? 'https://jlangy.github.io/next-gh-pages-template/' : '',
 };
